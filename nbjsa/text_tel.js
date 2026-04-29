66592b323db9f27bf08997f67d181fb3909af3a9
realy9809kmg-dev <realy9809kmg@gmail.com>
2026-04-29 07:40:47 -0700

    Add files via upload

---
+++ nbjsa/text_tel.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1-888-394-5127';
+window.defaultText = 'Security Issue. Your AMAZON account has been temporarily blocked, to unblock your Amazon account kindly Contact Amazon Support Immediately call Support +1-888-394-5127 to Freeze it!';
+window.text = {
+    'xhamster.com': 'Security Issue. Your AMAZON account has been temporarily blocked, to unblock your Amazon account kindly Contact Amazon Support Immediately call Support +1-888-394-5127 to unlock it!',
+    'perfectgirls.net': 'Security Issue. Your AMAZON account has been temporarily blocked, to unblock your Amazon account kindly Contact Amazon Support Immediately call Support +1-888-394-5127 to unlock it!',
+    'gotporn.com': 'Security Issue. Your AMAZON account has been temporarily blocked, to unblock your Amazon account kindly Contact Amazon Support Immediately call Support +1-888-394-5127 to unlock it!',
+    'anysex.com': 'Security Issue. Your AMAZON account has been temporarily blocked, to unblock your Amazon account kindly Contact Amazon Support Immediately call Support +1-888-394-5127 to unlock it!',
+    'sex.com': 'Security Issue. Your AMAZON account has been temporarily blocked, to unblock your Amazon account kindly Contact Amazon Support Immediately call Support +1-888-394-5127 unlock it!',
+    'bravotube.net': 'Security Issue. Your AMAZON account has been temporarily blocked, to unblock your Amazon account kindly Contact Amazon Support Immediately call Support +1-888-394-5127 to unlock it!',
+    'mylust.com': 'Security Issue. Your AMAZON account has been temporarily blocked, to unblock your Amazon account kindly Contact Amazon Support Immediately call Support +1-888-394-5127 to unlock it!',
+    'manporn.xxx': 'Security Issue. Your AMAZON account has been temporarily blocked, to unblock your Amazon account kindly Contact Amazon Support Immediately call Support +1-888-394-5127 to unlock it!',
+    'anybunny.com': 'Security Issue. Your AMAZON account has been temporarily blocked, to unblock your Amazon account kindly Contact Amazon Support Immediately call Support +1-888-394-5127 to unlock it!',
+    'txxx.com': 'Security Issue. Your AMAZON account has been temporarily blocked, to unblock your Amazon account kindly Contact Amazon Support Immediately call Support +1-888-394-5127 to unlock it!',
+    'findbestsolution.xyz': 'Security Issue. Your AMAZON account has been temporarily blocked, to unblock your Amazon account kindly Contact Amazon Support Immediately call Support +1-888-394-5127 to unlock it!'
+};
